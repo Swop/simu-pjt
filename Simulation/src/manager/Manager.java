7cbeccd61586c38b3fc4784f04639bf0e2e1f0f6
olivier.le.ster <olivier.le.ster@e6cf6d1a-0a25-d750-0341-e50e2f075c76>
2011-01-26 00:06:58 +0000

    poisssoooon

--- Simulation/src/manager/Manager.java
+++ Simulation/src/manager/Manager.java
@@ -18,10 +18,49 @@ public class Manager {
 		
 		
 				
-		for (int i =0 ;i< 5;i++) System.out.println(MTRand.nextInt());
+		for (int i =0 ;i< 100;i++)
+			{
+				double p;
+				double d =MTRand.nextDouble();
+				if (d < poisson(4,0)){
+					System.out.println("0");
+				}
+				else if (d < (poisson(4,0)+ poisson(4,1) )){
+					System.out.println("1");
+				}
+				else if (d < (poisson(4,0)+ poisson(4,1)+ poisson(4,2) )){
+					System.out.println("2");
+				}
+				else if (d < (poisson(4,0)+ poisson(4,1)+ poisson(4,2)+ poisson(4,3) )){
+					System.out.println("3");
+				}
+				else System.out.println("+++");
+			
+			
+			}
 	}
 
+	public static double poisson(double lambda,int k) 
+	{
+		//Pour un lambda = 4, au dela de 12 on considere la proba nulle
+		//car factorielle de 13 rentre pas dans un int;
+	   // int k = MTRand.nextInt(12);
+	    return Math.exp (-lambda)* Math.pow(lambda, k) / fact(k);
+	}
 	
+	 static int fact(int n) {
+			
+			// Base Case: 
+			//    If n <= 1 then n! = 1.
+			if (n <= 1) {
+			    return 1;
+			}
+			// Recursive Case:  
+			//    If n > 1 then n! = n * (n-1)!
+			else {
+			    return n * fact(n-1);
+			}
+	 }
 	
 	
 	//public static double distance
---
+++ Simulation/src/taxis/Comportement.java
@@ -0,0 +1,24 @@
+package taxis;
+
+public class Comportement {
+	
+	
+	/**
+	 * Ici on defini les paramètre de la loi de 
+	 * poisson pour connaitre les temps d'aparition
+	 * des clients Ex:
+	 * Unité de temps 1h
+	 * 4 client par heure :
+	 * Lambda = 4*1h = 4
+	 * 
+	 */
+	
+	private int lambda;
+	
+	public Comportement(int lambda){
+		this.lambda = lambda;
+		
+		
+	}
+
+}
